4a4c0b2f945a9aab57fdf20a51e61efb03fb41c1
Blaine Venturine <blaineventurine@gmail.com>
2022-01-13 23:38:16 -0600

    session restore mostly working

---
+++ lua/sessionable-library.lua
@@ -0,0 +1,132 @@
+local Config = {}
+local Lib = {
+  logger = {},
+  conf = {
+    log_level = false,
+    last_loaded_session = nil,
+  },
+  Config = Config,
+  _VIM_FALSE = 0,
+  _VIM_TRUE = 1,
+  SESSION_DIR = nil,
+}
+
+function Lib.setup(config)
+  Lib.conf = Config.normalize(config)
+end
+
+function Config.normalize(config, existing)
+  local conf = existing or {}
+  if Lib.is_empty_table(config) then
+    return conf
+  end
+
+  for k, v in pairs(config) do
+    conf[k] = v
+  end
+
+  return conf
+end
+
+local function has_value(tab, val)
+  for _, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+
+  return false
+end
+
+function Lib.get_file_name(url)
+  return url:match "^.+/(.+)$"
+end
+
+function Lib.get_file_extension(url)
+  return url:match "^.+(%..+)$"
+end
+
+function Lib.is_empty_table(t)
+  if t == nil then
+    return true
+  end
+  return next(t) == nil
+end
+
+function Lib.is_empty(s)
+  return s == nil or s == ""
+end
+
+function Lib.ends_with(str, ending)
+  return ending == "" or str:sub(-#ending) == ending
+end
+
+function Lib.append_slash(str)
+  if not Lib.is_empty(str) then
+    if not Lib.ends_with(str, "/") then
+      str = str .. "/"
+    end
+  end
+  return str
+end
+
+function Lib.validate_session_dir(session_dir)
+  if Lib.is_empty(session_dir) or vim.fn.expand(session_dir) == vim.fn.expand(Lib.SESSION_DIR) then
+    return Lib.session_dir
+  end
+
+  if not Lib.ends_with(session_dir, "/") then
+    session_dir = session_dir .. "/"
+  end
+
+  if vim.fn.isdirectory(vim.fn.expand(session_dir)) == Lib._VIM_FALSE then
+    vim.cmd(
+        "Path does not exist or is not a directory. "
+        .. string.format("Defaulting to %s.", Lib.session_dir)
+    )
+    return Lib.session_dir
+  else
+    Lib.logger.debug("Using custom session dir: " .. session_dir)
+    return session_dir
+  end
+end
+
+function Lib.init_dir(dir)
+  if vim.fn.isdirectory(vim.fn.expand(dir)) == Lib._VIM_FALSE then
+    vim.fn.mkdir(dir, "p")
+  end
+end
+
+function Lib.init_file(file_path)
+  if not Lib.is_readable(file_path) then
+    vim.cmd("!touch " .. file_path)
+  end
+end
+
+function Lib.is_readable(file_path)
+  local path, _ = file_path:gsub("\\%%", "%%")
+  local readable = vim.fn.filereadable(vim.fn.expand(path)) == Lib._VIM_TRUE
+
+  Lib.logger.debug("==== is_readable", readable)
+
+  return readable
+end
+
+function Lib.logger.debug(...)
+  if Lib.conf.log_level == "debug" then
+    print(...)
+  end
+end
+
+function Lib.logger.info(...)
+  local valid_values = { "info", "debug" }
+  if has_value(valid_values, Lib.conf.log_level) then
+    print(...)
+  end
+end
+
+function Lib.logger.error(...)
+  error(...)
+end
+
+return Lib
---
+++ lua/sessionable.lua
@@ -0,0 +1,163 @@
+local Lib = require "sessionable-library"
+
+local Sessionable = {
+  conf = {
+    auto_create_enabled = false,
+    auto_save_enabled = true,
+    enable_last_session = false,
+    log_level = "info",
+    session_dir = vim.fn.stdpath("config") .. "/sessions/",
+  },
+  session_name = "",
+  session_file_path = ""
+}
+
+function Sessionable.setup(config)
+  Sessionable.conf = Lib.Config.normalize(config, Sessionable.conf)
+  Lib.SESSION_DIR = Sessionable.conf.session_dir
+  Lib.setup {
+    log_level = Sessionable.conf.log_level,
+  }
+end
+
+do
+  function Sessionable.get_latest_session()
+    local dir = vim.fn.expand(Sessionable.conf.session_dir)
+    local latest_session = { session = nil, last_edited = 0 }
+
+    for _, filename in ipairs(vim.fn.readdir(dir)) do
+      local session = Sessionable.conf.session_dir .. filename
+      local last_edited = vim.fn.getftime(session)
+
+      if last_edited > latest_session.last_edited then
+        latest_session.session = session
+        latest_session.last_edited = last_edited
+      end
+    end
+  end
+end
+
+------ MAIN FUNCTIONS ------
+local function run_hook_cmds(cmds, hook_name)
+  if not Lib.is_empty_table(cmds) then
+    for _, cmd in ipairs(cmds) do
+      Lib.logger.debug(string.format("Running %s command: %s", hook_name, cmd))
+      local success, result
+
+      if type(cmd) == "function" then
+        success, result = pcall(cmd)
+      else
+        success, result = pcall(vim.cmd, cmd)
+      end
+
+      if not success then
+        Lib.logger.error(string.format("Error running %s. error: %s", cmd, result))
+      end
+    end
+  end
+end
+
+local function message_after_saving(path, auto)
+  if auto then
+    Lib.logger.debug("Session saved at " .. path)
+  else
+    Lib.logger.info("Session saved at " .. path)
+  end
+end
+
+function Sessionable.get_session_dir()
+  if Sessionable.validated then
+    return Sessionable.conf.session_dir
+  end
+
+  local session_dir = vim.g["session_dir"] or Sessionable.conf.session_dir
+  Lib.init_dir(session_dir)
+  Sessionable.conf.session_dir = Lib.validate_session_dir(session_dir)
+  Sessionable.validated = true
+  return session_dir
+end
+
+function Sessionable.get_cmds(type)
+  return Sessionable.conf[type .. "_cmds"]
+end
+
+
+-- Saves the session, overriding if previously existing.
+function Sessionable.SaveSession(sessions_dir, auto)
+  local pre_cmds = Sessionable.get_cmds("pre_save")
+  run_hook_cmds(pre_cmds, "pre-save")
+  vim.cmd("mks! " .. Sessionable.session_file_path)
+  message_after_saving(Sessionable.session_file_path, auto)
+  local post_cmds = Sessionable.get_cmds("post_save")
+  run_hook_cmds(post_cmds, "post-save")
+end
+
+function Sessionable.RestoreSession(session_name)
+  local restore = function()
+    local pre_cmds = Sessionable.get_cmds("pre_restore")
+    run_hook_cmds(pre_cmds, "pre-restore")
+
+    local cmd = "source " .. Sessionable.session_file_path 
+    local success, result = pcall(vim.cmd, cmd)
+
+    if not success then
+      Lib.logger.error([[
+        Error restoring session! The session might be corrupted.
+        Disabling auto save. Please check for errors in your config. Error: 
+      ]] .. result)
+      Sessionable.conf.auto_save_enabled = false
+      return
+    end
+
+    Lib.logger.info("Session restored from " .. Sessionable.session_file_path)
+    Lib.conf.last_loaded_session = Sessionable.session_file_path 
+
+    local post_cmds = Sessionable.get_cmds("post_restore")
+    run_hook_cmds(post_cmds, "post-restore")
+  end
+
+    Sessionable.session_file_path = string.format("%s%s", Sessionable.get_session_dir(), session_name)
+    if Lib.is_readable(Sessionable.session_file_path) then
+      Lib.logger.debug("isReadable, calling restore")
+      restore()
+      Sessionable.session_name = session_name
+    else
+      Lib.logger.debug("File not readable, not restoring session")
+    end
+end
+
+function Sessionable.DisableAutoSave()
+  Lib.logger.debug "Auto Save disabled"
+  Sessionable.conf.auto_save_enabled = false
+end
+
+function Sessionable.CompleteSessions()
+  local session_files = vim.fn.glob(Sessionable.get_session_dir() .. "/*", true, true)
+  local session_names = {}
+  for _, sf in ipairs(session_files) do
+    local name = Lib.unescape_dir(vim.fn.fnamemodify(sf, ":t:r"))
+    table.insert(session_names, name)
+  end
+  return table.concat(session_names, "\n")
+end
+
+function Sessionable.DeleteSession(session_name)
+  local pre_cmds = Sessionable.get_cmds("pre_delete")
+  run_hook_cmds(pre_cmds, "pre-delete")
+
+  if Sessionable.conf.auto_save_enabled then
+    Sessionable.DisableAutoSave()
+  end
+
+  local cmd = "silent! !rm " .. Sessionable.session_file_path
+  local success, result = pcall(vim.cmd, cmd)
+  if success then
+    Lib.logger.info("Deleted session " .. Sessionable.session_file_path)
+    local post_cmds = Sessionable.get_cmds("post_delete")
+    run_hook_cmds(post_cmds, "post-delete")
+  else
+    Lib.logger.error("Error deleting session " .. Sessionable.session_file_pathj)
+  end
+end
+
+return Sessionable
